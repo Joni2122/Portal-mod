404e585a43d2105070043908b76f6a95bd6f2737
Joni2122 <netherack@gmx.de>
2026-06-12 17:28:02 +0200

    Create PortalMod.java

---
+++ src/main/java/com/portalmod/PortalMod.java
@@ -0,0 +1,17 @@
+package com.portalmod;
+
+import net.fabricmc.api.ModInitializer;
+
+public class PortalMod implements ModInitializer {
+
+    @Override
+    public void onInitialize() {
+        System.out.println("Portal Mod loaded!");
+
+        // TODO:
+        // - Portal frame detection
+        // - Block-based dimension creation
+        // - Teleport system
+        // (next files will add real logic)
+    }
+}
